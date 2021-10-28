52384c2735061693b93b6cedfada61c8f122dd72
ZèD <imzihad@gmail.com>
2021-10-29 01:41:05 +0600

    Connection to database and cors setup
    Signed-off-by: ZèD <imzihad@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
+
+// Initialize App
+const app = express();
+dotenv.config();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Read from ENV
+const port = process.env.PORT || 9000;
+const uri = process.env.MONGODB_URI;
+
+// Connection to MongoDB
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const dbConnect = async () => {
+    try {
+        await client.connect();
+    }
+    finally {
+        console.log('Connection to MongoDB successfull');
+    }
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('API for Styla Rental App is LIVE!')
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Styla Rental app listening at port: ${port}`)
+});
